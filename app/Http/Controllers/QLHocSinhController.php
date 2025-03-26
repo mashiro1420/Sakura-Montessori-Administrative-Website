3512815ed86bbea2625c7ad2104dac0d1ad451a7
DoDucManh <mashiro1420@gmail.com>
2025-03-27 01:09:03 +0700

    be-update import

--- app/Http/Controllers/QLHocSinhController.php
+++ app/Http/Controllers/QLHocSinhController.php
@@ -4,38 +4,119 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\HocSinhModel;
+use App\Models\TaiKhoanModel;
 use Illuminate\Http\Request;
 
 class QLHocSinhController extends Controller
 {
-    public function viewQuanLy()
+    public function viewQuanLy(Request $request)
     {
-        $data=[];
-        $data['bao_loi'] = session('bao_loi');
-        $query = HocSinhModel::query()->select('*');
-		session()->put('bao_loi', '');
-        $data['hoc_sinhs'] = $query->get();
-        return view('Quan_ly_hoc_sinh.quan_ly_hoc_sinh',$data);//
+        $data = [];
+        $data['hoc_sinhs'] = HocSinhModel::all();
+        return view('Hoc_sinh.hoc_sinh', $data);
     }
-    public function viewThem()
+    public function viewChiTiet(Request $request)
     {
-        $data=[];
-        $data['bao_loi'] = session('bao_loi');
-		session()->put('bao_loi', '');
-        return view('Quan_ly_hoc_sinh.quan_ly_hoc_sinh',$data);//
+        $data = [];
+        $data['hoc_sinhs'] = HocSinhModel::all();
+        $data['hoc_sinh'] = HocSinhModel::find($request->id);
+        return view('Hoc_sinh.hoc_sinh', $data);
     }
-    public function viewSua()
+    public function viewThem(Request $request)
     {
-        $data=[];
-        $data['bao_loi'] = session('bao_loi');
-		session()->put('bao_loi', '');
-        return view('Quan_ly_hoc_sinh.quan_ly_hoc_sinh',$data);//
+        $data = [];
+        return view('Hoc_sinh.them_hoc_sinh', $data);
     }
-    public function viewDanSu()
+    public function viewSua(Request $request)
     {
-        $data=[];
-        $data['bao_loi'] = session('bao_loi');
-		session()->put('bao_loi', '');
-        return view('Quan_ly_hoc_sinh.quan_ly_hoc_sinh',$data);//
+        $data = [];
+        $data['hoc_sinh'] = HocSinhModel::find($request->id);
+        return view('Hoc_sinh.sua_hoc_sinh', $data);
+    }
+//-------------------------------------------------
+    public function xlThem(Request $request)
+    {
+        $all_hoc_sinh = HocSinhModel::all();
+        $so_hs = count($all_hoc_sinh);
+        $ma_hs = 'SB'.str_pad($so_hs+1,10,'0',STR_PAD_LEFT);
+        $hoc_sinh = new HocSinhModel();
+        $hoc_sinh->id = $ma_hs;
+        $hoc_sinh->ho_ten = $request->ho_ten;
+        $hoc_sinh->ngay_nhap_hoc = $request->ngay_nhap_hoc;
+        $hoc_sinh->ngay_thoi_hoc = $request->ngay_thoi_hoc;
+        $hoc_sinh->nickname = $request->nickname;
+        $hoc_sinh->gioi_tinh = $request->gioi_tinh;
+        $hoc_sinh->ngay_sinh = $request->ngay_sinh;
+        $hoc_sinh->quoc_tich = $request->quoc_tich;
+        $hoc_sinh->ngon_ngu = $request->ngon_ngu;
+        $hoc_sinh->can_nang = $request->can_nang;
+        $hoc_sinh->chieu_cao = $request->chieu_cao;
+        $hoc_sinh->noi_sinh = $request->noi_sinh;
+        $hoc_sinh->thong_tin_suc_khoe = $request->thong_tin_suc_khoe;
+        $hoc_sinh->di_bus = $request->di_bus;
+        $hoc_sinh->ho_ten_me = $request->ho_ten_me;
+        $hoc_sinh->sdt_me = $request->sdt_me;
+        $hoc_sinh->email_me = $request->email_me;
+        $hoc_sinh->nghe_nghiep_me = $request->nghe_nghiep_me;
+        $hoc_sinh->cmnd_me = $request->cmnd_me;
+        $hoc_sinh->nam_sinh_me = $request->nam_sinh_me;
+        $hoc_sinh->quoc_tich_me = $request->quoc_tich_me;
+        $hoc_sinh->ho_ten_bo = $request->ho_ten_bo;
+        $hoc_sinh->sdt_bo = $request->sdt_bo;
+        $hoc_sinh->email_bo = $request->email_bo;
+        $hoc_sinh->nghe_nghiep_bo = $request->nghe_nghiep_bo	;
+        $hoc_sinh->cmnd_bo = $request->cmnd_bo;
+        $hoc_sinh->nam_sinh_bo = $request->nam_sinh_bo;
+        $hoc_sinh->quoc_tich_bo = $request->quoc_tich_bo;
+        $hoc_sinh->thuong_tru = $request->thuong_tru;
+        $hoc_sinh->dia_chi = $request->dia_chi;
+        $hoc_sinh->nguoi_dua_don = $request->nguoi_dua_don;
+        $hoc_sinh->lien_he_khan = $request->lien_he_khan;
+        $hoc_sinh->loai_hoc_phi = $request->loai_hoc_phi;
+        $tai_khoan = new TaiKhoanModel();
+        $tai_khoan->tai_khoan = $ma_hs;
+        $tai_khoan->id_hoc_sinh = $ma_hs;
+        $tai_khoan->la_khach = true;
+        $tai_khoan->id_quyen = 2;
+        $hoc_sinh->save();
+        $tai_khoan->save();
+        return redirect()->route('ql_hs');
+    }
+    public function xlSua(Request $request)
+    {
+        $hoc_sinh = HocSinhModel::find($request->id);
+        $hoc_sinh->ho_ten = $request->ho_ten;
+        $hoc_sinh->ngay_nhap_hoc = $request->ngay_nhap_hoc;
+        $hoc_sinh->ngay_thoi_hoc = $request->ngay_thoi_hoc;
+        $hoc_sinh->nickname = $request->nickname;
+        $hoc_sinh->gioi_tinh = $request->gioi_tinh;
+        $hoc_sinh->ngay_sinh = $request->ngay_sinh;
+        $hoc_sinh->quoc_tich = $request->quoc_tich;
+        $hoc_sinh->ngon_ngu = $request->ngon_ngu;
+        $hoc_sinh->can_nang = $request->can_nang;
+        $hoc_sinh->chieu_cao = $request->chieu_cao;
+        $hoc_sinh->noi_sinh = $request->noi_sinh;
+        $hoc_sinh->thong_tin_suc_khoe = $request->thong_tin_suc_khoe;
+        $hoc_sinh->di_bus = $request->di_bus;
+        $hoc_sinh->ho_ten_me = $request->ho_ten_me;
+        $hoc_sinh->sdt_me = $request->sdt_me;
+        $hoc_sinh->email_me = $request->email_me;
+        $hoc_sinh->nghe_nghiep_me = $request->nghe_nghiep_me;
+        $hoc_sinh->cmnd_me = $request->cmnd_me;
+        $hoc_sinh->nam_sinh_me = $request->nam_sinh_me;
+        $hoc_sinh->quoc_tich_me = $request->quoc_tich_me;
+        $hoc_sinh->ho_ten_bo = $request->ho_ten_bo;
+        $hoc_sinh->sdt_bo = $request->sdt_bo;
+        $hoc_sinh->email_bo = $request->email_bo;
+        $hoc_sinh->nghe_nghiep_bo = $request->nghe_nghiep_bo	;
+        $hoc_sinh->cmnd_bo = $request->cmnd_bo;
+        $hoc_sinh->nam_sinh_bo = $request->nam_sinh_bo;
+        $hoc_sinh->quoc_tich_bo = $request->quoc_tich_bo;
+        $hoc_sinh->thuong_tru = $request->thuong_tru;
+        $hoc_sinh->dia_chi = $request->dia_chi;
+        $hoc_sinh->nguoi_dua_don = $request->nguoi_dua_don;
+        $hoc_sinh->lien_he_khan = $request->lien_he_khan;
+        $hoc_sinh->loai_hoc_phi = $request->loai_hoc_phi;
+        return redirect()->route('ql_hs');
     }
 }
--- app/Imports/NhanVienImport.php
+++ app/Imports/NhanVienImport.php
@@ -3,7 +3,9 @@
 namespace App\Imports;
 
 use App\Models\ChucVuModel;
+use App\Models\ChuyenNganhModel;
 use App\Models\NhanVienModel;
+use App\Models\TaiKhoanModel;
 use App\Models\TTBangCapModel;
 use App\Models\TTDanSuModel;
 use App\Models\TTHonNhanModel;
@@ -26,7 +28,7 @@ class NhanVienImport implements ToModel,WithHeadingRow
             'ho_ten' => $row['ho_ten'],
             'gioi_tinh' => $row['gioi_tinh'],
             'noi_sinh' => $row['noi_sinh'],
-            'id_chuc_vu' => ChucVuModel::where('ten_chuc_vu',$row['chuc_vu'])->first()->id,
+            'id_chuc_vu' => ChucVuModel::where('ten_chuc_vu','like','%'.$row['chuc_vu'].'%')->first()->id,
             'ngay_sinh' => $row['ngay_sinh'],
             'ngay_vao_lam' => $row['ngay_vao_lam'],
             'cmnd' => $row['so_cmnd_ho_chieu'],
@@ -43,7 +45,6 @@ class NhanVienImport implements ToModel,WithHeadingRow
             'so_con' => $row['so_con'],
         ],['id'], ['tinh_trang_hon_nhan', 'so_con']);
 
-        // Insert into tt_lienhe (Contact Information)
         TTLienHeModel::upsert([
             'id_nhan_vien' => $row['ma_nhan_vien'],
             'sdt_rieng' => $row['dt_di_dong'],
@@ -52,7 +53,6 @@ class NhanVienImport implements ToModel,WithHeadingRow
             'email_rieng' => $row['email_ca_nhan'],
         ],['id'], ['sdt_rieng', 'sdt_noi_bo', 'email_noi_bo', 'email_rieng']);
         
-        // Insert into tt_bangcap (Education)
         TTBangCapModel::upsert([
             'id_nhan_vien' => $row['ma_nhan_vien'],
             'trinh_do_hoc_van' => $row['trinh_do_hoc_van'],
@@ -62,12 +62,11 @@ class NhanVienImport implements ToModel,WithHeadingRow
             'chung_chi' => $row['chung_chi'],
             'montessori' => $row['mon_qt'],
             'truong_dao_tao' => $row['truong_dao_tao'],
-            // 'chuyen_nganh' => $row['chuyen_nganh'],
+            'chuyen_nganh' => ChuyenNganhModel::where('ten_chuyen_nganh','like','%'.$row['chuyen_nganh'].'%')->first()->id,
             'xep_loai' => $row['xep_loai_tdcm'],
             'nam_tot_nghiep' => $row['nam_tot_nghiep'],
         ],['id'], ['trinh_do_hoc_van', 'trinh_do_chuyen_mon', 'trinh_do_chinh', 'hinh_thuc_dao_tao', 'chung_chi', 'montessori', 'truong_dao_tao', 'xep_loai', 'nam_tot_nghiep']);
-        
-        // Insert into tt_dansu (Personal Information)
+      
         TTDanSuModel::upsert([
             'id_nhan_vien' => $row['ma_nhan_vien'],
             'so_bhxh' => $row['so_so_bhxh'],
@@ -78,7 +77,6 @@ class NhanVienImport implements ToModel,WithHeadingRow
             'khai_sinh' => $row['dia_chi_khai_sinh'],
         ],['id'], ['so_bhxh', 'thang_tham_gia_bhxh', 'ma_so_thue', 'thuong_tru', 'tam_tru', 'khai_sinh']);
 
-        // Insert into tt_hopdong (Contracts)
         TTHopDongModel::upsert([
             'id_nhan_vien' => $row['ma_nhan_vien'],
             'loai_hd' => $row['loai_hop_dong'],
@@ -86,5 +84,11 @@ class NhanVienImport implements ToModel,WithHeadingRow
             'ngay_ky' => $row['ngay_ky_hop_dong'],
             'ngay_ket_thuc' => $row['ngay_ket_thuc_hop_dong'],
         ],['id'], ['loai_hd', 'so_hd', 'ngay_ky', 'ngay_ket_thuc']);
-    }
+        TaiKhoanModel::upsert([
+            'tai_khoan' => $row['ma_nhan_vien'],
+            'id_nhan_vien' => $row['ma_nhan_vien'],
+            'la_khach' => false,
+            'id_quyen' => 3,
+        ],['tai_khoan'], ['id_nhan_vien', 'la_khach', 'id_quyen']);
+    }   
 }
